056057b839a13e497a689b751299183e1a60d107
Max <79218470+snowy-connection@users.noreply.github.com>
2024-07-24 00:57:43 +0200

    Fix typos and logic errors in main.py

--- API/main.py
+++ API/main.py
@@ -1,6 +1,6 @@
 import network
 
-from external import micropyserver.MicroPyServer
+from external.micropyserver import MicroPyServer
 from external import utils
 
 class HTTP_Server:
@@ -10,7 +10,7 @@ class HTTP_Server:
         # Static files routes
         self._server.add_route("/", self._index_handler)
         self._server.add_route("/burgtor.css", self._index_css_handler)
-        hself._server.add_route("/burgtor.js", self._index_js_handler)
+        self._server.add_route("/burgtor.js", self._index_js_handler)
 
         # Open door route
         self._server.add_route("/open-door", self._open_door_handler, method="POST")
@@ -18,19 +18,19 @@ class HTTP_Server:
         # Read files
         self._filecontents = {}
         for filename in ["burgtor.html", "burgtor.css", "burgtor.js"]:
-            with f = open(f"/Captive_Portal/{filename}", "r"):
+            with open(f"/Captive_Portal/{filename}", "r") as f:
                 self._filecontents[filename] = f.read()
 
-    def _index_handler(self):
+    def _index_handler(self, request):
         utils.send_response(self._server, self._filecontents["burgtor.html"])
 
-    def _index_css_handler(self):
+    def _index_css_handler(self, request):
         utils.send_response(self._server, self._filecontents["burgtor.css"], content_type="text/css")
 
-    def _index_js_handler(self):
+    def _index_js_handler(self, request):
         utils.send_response(self._server, self._filecontents["burgtor.js"], content_type="text/js")
 
-    def _open_door_handler(self):
+    def _open_door_handler(self, request):
         # TODO
         utils.send_response(self._server, "", http_code=204)
 
@@ -39,10 +39,15 @@ class HTTP_Server:
 
 def main():
     # Open an access point
+    network.hostname("burgtor")
+
+    print("Creating a Hot Spot")
     ap = network.WLAN(network.AP_IF)
     ap.active(True)
-    with f = open("/WIFI_SECRET", "r"):
-        password = f.read()
+    with open("/WIFI_SECRET", "r") as f:
+        password = f.readline().split("\n")[0]
+
+    print("You can connect with SSID 'Burgtor' and Password '" + password + "'")
     ap.config(essid="Burgtor", password=password)
 
     # Start the HTTP-Server
